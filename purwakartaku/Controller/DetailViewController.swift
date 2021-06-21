4e39ec58cf917fc25bdd16949fe9e71fc8055edf
Oratakashi Nhamako <davidcreazy@gmail.com>
2021-06-21 17:32:24 +0700

    Add Sogue to Detail

--- purwakartaku/Controller/DetailViewController.swift
+++ purwakartaku/Controller/DetailViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    var hotel: DataHotel? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +18,6 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- purwakartaku/Controller/ViewController.swift
+++ purwakartaku/Controller/ViewController.swift
@@ -15,10 +15,20 @@ class ViewController: UIViewController {
     
     var data :Array = [DataHotel]()
     
+    let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         rvHotel.dataSource = self
         rvHotel.register(UINib(nibName: "HotelTableViewCell", bundle: nil), forCellReuseIdentifier: "HotelTableViewCell")
+        rvHotel.delegate = self
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating();
+        alert.view.addSubview(loadingIndicator)
+        
         getHotel(){ (it) in
             switch it {
             case .failure(let error):
@@ -38,7 +48,10 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt position: IndexPath) {
+        let detail = DetailViewController(nibName: "DetailViewController", bundle: nil)
+        detail.hotel = data[position.row]
+        performSegue(withIdentifier: "toDetail", sender: self)
     }
 }
 
@@ -62,12 +75,14 @@ extension ViewController: UITableViewDataSource {
 extension ViewController {
     func getHotel(completion: @escaping (Result<ResponseHotel, Error>) ->()) {
         let headers: HTTPHeaders = ["Accept": "application/json"]
-        
+        present(alert, animated: true, completion: nil)
         AF.request(Config().BASE_URL + "hotel", headers: headers).responseDecodable(of: ResponseHotel.self){ (result) in
             if let error = result.error {
+                self.dismiss(animated: false, completion: nil)
                 completion(.failure(error))
             }
             guard result.response!.statusCode == 200 else { return }
+            self.dismiss(animated: false, completion: nil)
             completion(.success(result.value!))
         }
     }
